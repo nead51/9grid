379ac72607b746bd72780d33dd177b1633b9c7c0
dustin.nead <dustin.nead@gmail.com>
2019-06-14 20:49:01 -0400

    Changes to be committed: modified:   index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <CylinoidSVG />
+      
     </div>
   );
 }
