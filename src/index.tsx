3703880198d1bd8d6f6125c6c63afe32e90ed97f
nead51 <Dustin.nead@gmail.com>
2019-06-16 21:17:21 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './App';
+import 'App.less';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
