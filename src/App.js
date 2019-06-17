467c5dc945bc28f6e8e2d96f0653d2eea4aabcc0
nead51 <Dustin.nead@gmail.com>
2019-06-16 22:17:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.less';
-import './FusionBox.js';
+import FusionBox './FusionBox.js';
 
 
 class App extends Component {
